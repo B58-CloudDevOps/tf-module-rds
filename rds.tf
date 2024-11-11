76ec6a436bb86a5e74db51a1ee8f2e6f84e8568e
CodingManoj <CodingManoj@gmail.com>
2024-11-10 22:35:27 -0500

    Kubernetes AutoPush

--- rds.tf
+++ rds.tf
@@ -9,6 +9,7 @@ resource "aws_db_instance" "main" {
   username               = jsondecode(data.vault_generic_secret.rds.data_json).username
   parameter_group_name   = aws_db_parameter_group.main.name
   skip_final_snapshot    = true
+  db_subnet_group_name   = aws_db_subnet_group.main.name
   vpc_security_group_ids = [aws_security_group.main.id]
 }
 
